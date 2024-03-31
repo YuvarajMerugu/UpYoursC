8636a41178ac8430e2bd3c3c3abecc5d14ba55c2
Yuvaraj Merugu <yuvarajmerugu712@gmail.com>
2024-04-01 02:52:41 +0530

    new changes

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -11,7 +11,7 @@ const HeroBanner = ({ heroBanner }) => {
 
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+        {/* <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" /> */}
 
         <div>
           <Link href={`/aboutus`}>
